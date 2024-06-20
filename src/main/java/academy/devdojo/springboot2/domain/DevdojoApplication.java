d1c240c159adae78d78e1cf7d08c0b817ab56e43
patrick-andrade-flausino <patrickandradeflausino@gmail.com>
2024-06-19 23:19:29 -0300

    commit inicial: iniciando o repositorio para videoaula

---
+++ src/main/java/academy/devdojo/springboot2/domain/DevdojoApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.springboot2.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdojoApplication.class, args);
+	}
+
+}
