e42d1067bd2d291417d88e41aecd67ff061e7c27
patrick-andrade-flausino <patrickandradeflausino@gmail.com>
2024-06-19 23:42:06 -0300

    component scan

--- src/main/java/academy/devdojo/springboot2/DevdojoApplication.java
+++ src/main/java/academy/devdojo/springboot2/DevdojoApplication.java
@@ -1,9 +1,11 @@
-package academy.devdojo.springboot2.start;
+package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "academy.devdojo.springboot2")
 public class DevdojoApplication {
 
 	public static void main(String[] args) {
